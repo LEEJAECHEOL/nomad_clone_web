d30b9945495fd6a663f72326b07c5f06a8f9abec
DongTaeWan <bdj42@naver.com>
2021-03-30 15:08:24 +0900

    Tech Upload

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -9,6 +9,7 @@ import dashBoardSaga from "./dashboard";
 import categorySaga from "./category";
 import coursesSaga from "./courses";
 import videoSaga from "./video";
+import techSaga from "./admin/courses/tech";
 axios.defaults.baseURL = "http://localhost:8080";
 axios.defaults.headers.post["Content-Type"] = "application/json; charset=utf-8";
 
@@ -21,6 +22,7 @@ export default function* rootSaga() {
     fork(categorySaga),
     fork(dashBoardSaga),
     fork(adminCoursesSaga),
+    fork(techSaga),
     fork(adminVideoSaga),
     fork(videoSaga),
   ]);
