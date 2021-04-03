1cd98d9912cfb24120b22eab43b740c0ab1c4b4d
DongTaeWan <bdj42@naver.com>
2021-04-03 15:06:01 +0900

    Dashboard Payment, Purchase Course

--- src/reducers/pay.js
+++ src/reducers/pay.js
@@ -5,11 +5,17 @@ export const initialState = {
   payPostLoading: false,
   payPostDone: false,
   payPostError: null,
+
   payGetLoading: false,
   payGetDone: false,
   payGetError: null,
 
-  payLost: [],
+  userPayGetLoading: false,
+  userPayGetDone: false,
+  userPayGetError: null,
+
+  userPayList: [],
+  payList: [],
 };
 
 export const PAY_POST_REQUEST = "PAY_POST_REQUEST";
@@ -19,10 +25,13 @@ export const PAY_POST_FAILURE = "PAY_POST_FAILURE";
 export const PAY_GET_REQUEST = "PAY_GET_REQUEST";
 export const PAY_GET_SUCCESS = "PAY_GET_SUCCESS";
 export const PAY_GET_FAILURE = "PAY_GET_FAILURE";
+
+export const USER_PAY_GET_REQUEST = "USER_PAY_GET_REQUEST";
+export const USER_PAY_GET_SUCCESS = "USER_PAY_GET_SUCCESS";
+export const USER_PAY_GET_FAILURE = "USER_PAY_GET_FAILURE";
 // 커뮤니티
 
 // 액션
-
 export const payPostRequestAction = (data) => {
   return {
     type: PAY_POST_REQUEST,
@@ -36,10 +45,17 @@ export const payGetRequestAction = (data) => {
   };
 };
 
+export const userPayGetRequestAction = (data) => {
+  return {
+    type: USER_PAY_GET_REQUEST,
+    data,
+  };
+};
+
 const reducer = (state = initialState, action) => {
   return produce(state, (draft) => {
     switch (action.type) {
-      // get
+      // 결제하기
       case PAY_POST_REQUEST:
         draft.payPostLoading = true;
         draft.payPostDone = false;
@@ -55,6 +71,8 @@ const reducer = (state = initialState, action) => {
         draft.payPostLoading = false;
         draft.payPostError = action.error;
         break;
+
+      // 어드민 결제목록
       case PAY_GET_REQUEST:
         draft.payGetLoading = true;
         draft.payGetDone = false;
@@ -70,6 +88,24 @@ const reducer = (state = initialState, action) => {
       case PAY_GET_FAILURE:
         draft.payGetLoading = false;
         draft.payGetError = action.error;
+        break;
+
+      // 대시보드페이지 결제목록
+      case USER_PAY_GET_REQUEST:
+        draft.userPayGetLoading = true;
+        draft.userPayGetDone = false;
+        draft.userPayGetError = null;
+        break;
+
+      case USER_PAY_GET_SUCCESS:
+        draft.userPayGetLoading = false;
+        draft.userPayGetDone = true;
+        draft.userPayList = action.data;
+        break;
+
+      case USER_PAY_GET_FAILURE:
+        draft.userPayGetLoading = false;
+        draft.userPayGetError = action.error;
         break;
 
       default:
--- src/sagas/pay.js
+++ src/sagas/pay.js
@@ -3,14 +3,23 @@ import { push } from "connected-react-router";
 import axios from "axios";
 
 import {
+  // 결제하기
   PAY_POST_FAILURE,
   PAY_POST_REQUEST,
   PAY_POST_SUCCESS,
+
+  // 대시보드페이지 결제목록
+  USER_PAY_GET_FAILURE,
+  USER_PAY_GET_REQUEST,
+  USER_PAY_GET_SUCCESS,
+
+  // 어드민 결제목록확인
   PAY_GET_FAILURE,
   PAY_GET_REQUEST,
   PAY_GET_SUCCESS,
 } from "../reducers/pay";
 
+// 결제하기
 function payPostAPI(data) {
   const config = {
     headers: {
@@ -43,6 +52,34 @@ function* payPost(action) {
   }
 }
 
+// 대시보드페이지 결제목록
+function userPayGetAPI(data) {
+  const config = {
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem("nomadToken"),
+    },
+  };
+  console.log("여기들어옴?", data);
+  return axios.get(`/pay/${data}`, config);
+}
+
+function* userPayGet(action) {
+  try {
+    const result = yield call(userPayGetAPI, action.data);
+    const data = result.data.data;
+    yield put({
+      type: USER_PAY_GET_SUCCESS,
+      data: data,
+    });
+  } catch (err) {
+    yield put({
+      type: USER_PAY_GET_FAILURE,
+      error: "결제 정보 저장 실패",
+    });
+  }
+}
+
+// 어드민 결제목록
 function payGetAPI() {
   const config = {
     headers: {
@@ -71,10 +108,15 @@ function* payGet() {
 function* watchPayPost() {
   yield takeLatest(PAY_POST_REQUEST, payPost);
 }
+
 function* watchPayGet() {
   yield takeLatest(PAY_GET_REQUEST, payGet);
 }
 
+function* watchUserPayGet() {
+  yield takeLatest(USER_PAY_GET_REQUEST, userPayGet);
+}
+
 export default function* paySaga() {
-  yield all([fork(watchPayPost), fork(watchPayGet)]);
+  yield all([fork(watchPayPost), fork(watchPayGet)], fork(watchUserPayGet));
 }
